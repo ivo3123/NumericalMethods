e936baca7eab83f103bcf6b250504bf6677f0516
ivo3123 <115104205+ivo3123@users.noreply.github.com>
2024-05-01 12:11:10 +0300

    Update tschebyscheff.py

--- util/tschebyscheff.py
+++ util/tschebyscheff.py
@@ -1,20 +1,19 @@
 import numpy as np
 
-def tschebyscheff(degree, k, start, end):
-    return ((start + end) / 2) + ((end - start) / 2) * np.cos((2*k - 1) / (2*degree) * np.pi)
+def tschebyscheff(n, k, start, end):
+    return ((start + end) / 2) + ((end - start) / 2) * np.cos((2*k - 1) / (2*n) * np.pi)
 
-def get_tschebyscheff_nodes(degree, start=-1, end=1):
+def get_tschebyscheff_nodes(nodes_count, start=-1, end=1):
     """
-    Returns the Tschebyscheff nodes in the interval [@start; @end] used for constructing an interpolating polynomial
-    Note that we need (n + 1) nodes to construct a polynomial of degree n, but we only need n Tschebyscheff nodes to to construct a polynomial of degree n.
+    Returns the Tschebyscheff nodes in the interval [@start; @end] used for constructing an interpolation polynomial
     The default interval in which the Tschebyscheff nodes are located is [-1, 1] but that can be ajusted with @start and @end.
 
     Args:
-        degree (int): the desired degree of the interpolated polynomial
+        nodes_count (int): the desired count of Tschebyscheff nodes
         start [optional] (float): [default is -1] start of the interval from which the nodes to be selected
         end [optional] (float): [default is 1] end of the interval from which the nodes to be selected
 
     Returns:
-        np.array of floats: @degree number of nodes in the interval [@start; @end]
+        np.array of floats: @nodes_count number of nodes in the interval [@start; @end]
     """
-    return np.array([tschebyscheff(degree, k, start, end) for k in range(1, degree+1)])
+    return np.array([tschebyscheff(nodes_count, k, start, end) for k in range(0, nodes_count)])
